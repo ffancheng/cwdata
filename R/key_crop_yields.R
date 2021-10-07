8e3f3eb50382f1f5362caf623575ae5d54a6b60a
Fan Cheng <Fan.Cheng@monash.edu>
2021-10-07 20:35:39 +1100

    Add function documentation

---
+++ R/key_crop_yields.R
@@ -0,0 +1,16 @@
+#' Key Crop Yields Dataset
+#'
+#' @section Background:
+#' The data was collected from ... over the course.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+#'
+#' @example
+#' print(key_crop_yields)
+"key_crop_yields"
